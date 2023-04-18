c82523383b7ba43eeda82a94611632a01d6854c4
davron20005 <128698498+davron20005@users.noreply.github.com>
2023-04-18 14:01:17 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,252 @@
+// var a  = prompt ("a asonini kiritng")   
+
+
+
+
+
+
+// var b  = prompt ("b asonini kiritng")
+// var c = prompt (" asonini kiritng")
+
+// if(c  == "+")
+// {
+// console.log(a+b);
+// }
+// else  if (c == "-")
+// {
+//     console.log(a-b);
+// }
+// else if (c == "*")
+// {
+//     console.log(a*b);
+// }
+
+// var age = +prompt("yoshingiz nechida")
+// if(age > 15){
+// console.log(2023 + age);}
+// else if(age < 15){
+//     console.log(2023 -15);
+// }
+
+// var age = +prompt("ixtiyoriy son kiriting")
+
+// if(age>0)
+// {console.log(age + 1);}
+// else if (age < 0)
+// {console.log(age - 1);}
+// else if (age ==0){
+// console.log(age);}
+
+// var a = prompt ("ixtiyoriy son kiriting")
+
+// if (a % 2== 0)
+// {console.log(true);}
+// else if ( a % 2 !=0)
+// {console.log(false);}
+
+// 1-misol
+
+// var a = +prompt ("butun son berilgan ")
+// if (a+1 ){
+//     console.log("1ga oshrirldi");
+// }
+// else if (a1){
+//     console.log("ozgarmadi");
+// }
+
+// 2-misol
+
+// var a = prompt ("butun son berilga")
+// if(a<0){console.log(a+1);}
+// else if(a>0){console.log(a-2);}
+
+// 3-misol
+
+// var a = prompt ("butun son berilgan")
+// if(a+1){console.log(a+1);}
+// else if (a>0){console.log(a-2);}
+// else if (a==0){console.log(10);}
+
+// 4-misol
+
+// var a = +prompt("son kiriting")
+// var b = +prompt("son kiriting")
+// var c = + prompt("son kiriting")
+
+// if (a > 0 && b > 0 && c > 0) { console.log("3ta musbat"); }
+// else if ((a < 0 && b > 0 && c > 0) || (a > 0 && b > 0 && c < 0) || (a > 0 && b < 0))
+
+// var a = 3
+// var b = 15
+
+// for (var i=b ; i<=a; i++ )console.log(i);
+
+// for (var i = 0; i<10; i++){
+//     if(i % 2 ==1)
+//     {continue}
+
+//     console.log(i);
+// }
+// for (var i = 0; i<10; i++){
+//     if(i  ==5)
+//     {break}
+
+//     console.log(i);
+// }
+
+// var n = 100
+// var summa = 0
+// for (var i =1 ; i<=n; i++){
+//     summa +=1
+// }
+// console.log(summa);
+// function summaNumber(n, summa = 0) {
+//   for (var i = 1; i <= n; i++) {
+//     summa += i;
+//   }
+//   return summa 
+// }
+// console.log(summaNumbe
+
+
+
+// var res [22,34,84,120];
+// function maxArrayElement(arr){var max = arr [0];
+//     for(var i=0; i<arr.length;i++)
+//     {if (arr[i]>max){max = arr[i]}}
+//     return max
+// }
+
+// console.log(maxArrayElement (res));
+
+// var res [22,19,90,5,43];
+// var summa = 0
+// for (var i = 0 ; i < res .length; i++){
+//     summa +=res[i]
+// }
+// console.log(summa);
+// var text = `mening vatanium Ozbekiston`
+// var array = text.split[0]
+// for (var i = 0; i < array.length; i++) 
+// {
+// if (array[i], length > max.length) {
+//  max = array[i]}
+// }
+// console.log(max);
+
+
+
+// var str = document.getElementById("title")
+// str.style.color = "green"
+// str.style.display ="flex"
+// str.style.backgroundColor="red"
+// console.log(str);
+
+
+// var str = document .getElementById("title")
+// str.style.color=("red")
+// str.style.display=("flex")
+// str.style.backgroundColor=("black")
+// console.log(str)
+
+
+
+
+// var btn = document.getElementById("btn")
+// var count = document.getElementById("count")
+// var btn1 = document.getElementById("btn1")
+// var btn2 = document.getElementById("btn2")
+// btn.addEventListener("click", function () {
+//     var item = count.textContent
+//     count.textContent = Number(item) +1
+// })
+
+// btn1.addEventListener("click", function () {
+//     var item = count.textContent
+//     count.textContent = Number(item) -1
+// })
+// btn2.addEventListener("click", function () {
+//     var item = count.textContent
+//     count.textContent  =0
+// })
+
+// var countElement = document.getElementById("count")
+// var inputElement = document.getElementById("input")
+// var resultsElement = document.getElementById("result")
+
+// resultsElemenet.addEventListener("click", function () {
+//     var current = +inputElement.value
+//     countElement.textContent = +countElement.textContent + current
+//     inputElement.value = ""
+// })
+
+// var wrapperElement = document.getElementsByClassName("wrapper")
+// var blackbutton = document.getElementsByClassName("black")
+// var redbutton = document.getElementsByClassName("wrapper")
+// redbutton.addEventListener("click", function () {
+// wrapperElement.classLisst.add("red")
+// wrapperElement.classLisst.remove("black")
+
+
+// }
+// )
+// blackbutton.addEventListener("click", function () {
+//     wrapperElement.classList.add("black")
+//     wrapperElement.classList.remove("red")
+
+
+
+// })
+// redbutton.addEventListener("click", function(){
+//     wrapperElement.classList.add("red")
+//     wrapperElement.classList.remove("black")
+// })
+
+// var n =  90
+// if( n % 3 ==0 ){console.log("Fizz");}
+
+// else( n % 5 !==0){console.log("Buzz");}
+
+
+// var n = 3
+// if(n < 0){console.log("Hello");}
+// else if (n > 0){console.log("welcome");}
+// else{console.log(0);}
+
+
+
+// var age = 3
+
+// if(age>0)
+// {console.log("juft");}
+// else if (age < 0)
+// {console.log("toq");}
+// else if (age ==0){
+// console.log(age);}
+// if ( age % 2 ==0){console.log("juft ham toq ham emas");}
+//  else if(age > 0){console.log("juft");}
+// else if ( age % 2==1 ){console.log("toq");}
+ 
+
+
+// var n =  15
+
+//  if( n % 3 == 0 && n % 5 == 0 ){console.log("FizzBuzz");}
+
+//  else if ( n % 3 ==0 )
+//  {console.log("Fizz");}
+// else if ( n % 5 ==0){console.log("buzz");}
+
+
+
+
+
+// Alisher => sayohat;
+// bilet => 500$;  // dollar;
+// museum => 120 euro;
+// Hotel => 200$ // dollar;
+
+// Alisher prompt dan o'zida bor uzbek somini kiritadi;
+// agar Alisher puli yetsa: oq yo'l Alisher;
+// aks xolda Ozgina sabr qilish kerak ekan;
+
